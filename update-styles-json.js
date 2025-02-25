d45c467b32c1f70ce54cceae0b59bc065eadc3b3
Sameera Sandakelum <sameerasw.com@gmail.com>
2025-02-25 14:07:30 +0530

    Update update-styles-json.js

--- update-styles-json.js
+++ update-styles-json.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const stylesDir = path.join(__dirname, 'styles');
+const rootDir = path.join(__dirname);
 const outputFile = path.join(__dirname, 'styles.json');
 
 const excludeFile = 'userChrome.css';
@@ -9,9 +9,9 @@ const excludeFile = 'userChrome.css';
 function updateStylesJson() {
   const styles = {};
 
-  fs.readdirSync(stylesDir).forEach(file => {
+  fs.readdirSync(rootDir).forEach(file => {
     if (file.endsWith('.css') && file !== excludeFile) {
-      const content = fs.readFileSync(path.join(stylesDir, file), 'utf-8');
+      const content = fs.readFileSync(path.join(rootDir, file), 'utf-8');
       styles[file] = content;
     }
   });
